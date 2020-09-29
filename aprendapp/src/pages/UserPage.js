1b687f35b2ad946eaee85f23cb9e3c67811c6cdc
Martin Brian <martin.brian1998@gmail.com>
2020-09-29 12:39:28 -0300

    agregado mapa con landmarks

--- aprendapp/src/pages/UserPage.js
+++ aprendapp/src/pages/UserPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { useParams } from "react-router-dom";
+import MapView from '../components/MapView.js';
 
 export default function UserPage() {
 
@@ -8,12 +9,19 @@ let { id } = useParams();
 
 return (
 
+    
+
 <>
 
-<h1>Hello there user {id}</h1>
 
-<p>This is your awesome User Profile page</p>
-<Link to="/"> HomePage </Link>
+    <h1>Hello there user {id}</h1>
+
+    <p>This is your awesome User Profile page</p>
+
+    <Link to="/"> HomePage </Link>
+
+    <MapView/>
+
 </>
 
 );
